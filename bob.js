08cf0fe1c72aa36e3645a028cc56670b15ac2f7a
AndyArora <74389174+AndyArora@users.noreply.github.com>
2021-02-18 20:46:17 -0500

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,23 @@
+class Bob{
+    constructor(x, y,r) {
+        var options = {
+            isStatic: false,
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y,r,options);
+        this.r = r 
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        fill("pink")
+        ellipseMode(RADIUS);
+        ellipse(0, 0,this.r, this.r);
+        pop();
+      }
+}
